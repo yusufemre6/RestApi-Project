3d290d836b9d01a758d65d69826476e9dba5af17
Yusuf Emre <yusufemre74ye6@gmail.com>
2023-07-22 18:29:55 +0300

    hata düzeltildi

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,45 +3,38 @@ const Token = require('../models/token');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const authMiddleware = (req, res, next) => {
-  // Bearer Token kontrolü
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  // Token doğrulama
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
+const authMiddleware = async (req, res, next) => {
+  try {
+    // Bearer Token kontrolü
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return res.status(401).json({ message: 'Unauthorized' });
     }
 
-    // Kullanıcının rolünü kontrol etme
-    if (decoded.role !== 'Admin') {
-      return res.status(403).json({ message: 'Forbidden' });
-    }
- 
-    Token.findOne({ value: token })
-    .then(foundToken => {
-      if (foundToken) {
-        const status = foundToken.status;
-        if(status){
-          req.user = decoded;
-        }
-        else{
-          return res.status(401).json({ message:'You are no longer active' });
-        }
-      } else {
-        console.log('Token bulunamadı.');
+    const token = authHeader.split(' ')[1];
+
+    // Token doğrulama
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
       }
-    })
-    .catch(err => {
-      console.error('Sorgu hatası:', err);
-    });   
+
+      // Kullanıcının rolünü kontrol etme
+      if (decoded.role !== 'Admin') {
+        return res.status(403).json({ message: 'Forbidden' });
+      }
+      req.user = decoded;
+    });
+    
+    const foundUser = await Token.findOne({ value: token });
+    if (foundUser.status===false) {
+      return res.status(403).json({ message: 'You are not longer than active' });
+    }
+
     next();
-  });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 module.exports = authMiddleware;
